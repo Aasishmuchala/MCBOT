720e83c57483a2b1b40b0d53a9a8f0ee4607cb49
Aasish Muchala <Aasishmuchala@gmail.com>
2026-02-03 14:11:10 +0530

    Fix Windows PATH issue in build script

--- build_app.bat
+++ build_app.bat
@@ -4,16 +4,17 @@ echo   Building TextureGen Pro for Windows
 echo ===================================================
 
 echo [1/3] Installing Build Tools...
-pip install -r requirements_app.txt
+python -m pip install -r requirements_app.txt
 
 echo.
 echo [2/3] Compiling to .EXE (This may take a minute)...
-pyinstaller --noconfirm --onefile --windowed --name "TextureGenPro" --add-data "scripts/texture_engine.py;." --hidden-import "PIL._tkinter_finder"  scripts/app_gui.py
+:: Using python -m PyInstaller is safer on Windows (avoids PATH issues)
+python -m PyInstaller --noconfirm --onefile --windowed --name "TextureGenPro" --add-data "scripts/texture_engine.py;." --hidden-import "PIL._tkinter_finder"  scripts/app_gui.py
 
 echo.
 echo [3/3] Cleanup...
-rmdir /s /q build
-del TextureGenPro.spec
+if exist build rmdir /s /q build
+if exist TextureGenPro.spec del TextureGenPro.spec
 
 echo.
 echo ===================================================
